889a25fd65f77b72f3406424ab15e1d3dabb7139
John Doe <jack.h.sawyer@pm.me>
2024-06-19 21:10:00 -0500

    first commit

---
+++ run.py
@@ -0,0 +1,20 @@
+# Import required libraries
+import seaborn as sns
+import pandas as pd
+
+print(pd.__version__)
+print(sns.__version__)
+
+# TODO: Load the Titanic dataset
+titanic_df = sns.load_dataset('titanic')
+
+# TODO: Filter data for third-class passengers who survived and are older than 40
+filtered_data = titanic_df[
+    (titanic_df['survived'] == 1) & (titanic_df['age'] > 40) & (titanic_df['pclass'] == 3)
+]
+
+# TODO: Sort the filtered data by age in ascending order
+sorted_data = filtered_data.sort_values(['age'], ascending=[True])
+
+# TODO: Print the sorted data
+print(sorted_data)
